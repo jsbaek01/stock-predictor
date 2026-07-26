3663de0ea066ea6ecd03875c3b212fd1dfe82a2a
jsbaek01 <65752766+jsbaek01@users.noreply.github.com>
2026-07-26 16:30:43 +0900

    Update index.py

--- api/index.py
+++ api/index.py
@@ -142,7 +142,7 @@ def get_live_financial_data(stock_code):
     
     # 보안 가짜 가면(Headers) 세팅 완벽 유지
     headers = {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
     }
     
